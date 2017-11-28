b5337c94000eccb95370c09c180ed7f2d0986d27
Defta Pradana Yuana <aldisa.defta@yahoo.com>
2017-11-28 21:15:52 +0700

    tambah script "iat"

---
+++ Game/Assets/CUT_BRAY/iat.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void Update () {
+		System.Console.Write ("GUGUK !, MEONG !");	
+	}
+}
