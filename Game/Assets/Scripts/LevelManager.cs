560e3681ef2bf4f5127c1abdb4bf4592ddf4f94f
krlan2789 <33871129+krlan2789@users.noreply.github.com>
2017-11-29 20:12:55 +0700

    Tambah LevelMenu v2

---
+++ Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public Button[] levelBtn;
+
+    void Start()
+    {
+        for (int i = 0; i < levelBtn.Length; i++)
+        {
+            levelBtn[i].interactable = false;
+        }
+    }
+
+    public void LoadLevel(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
