277982aa8b021a8d648aa1a15de2467a1ee1e089
krlan2789 <33871129+krlan2789@users.noreply.github.com>
2017-12-01 22:39:15 +0700

    Cuma Update

--- Game/Assets/Resources/Scripts/CameraFollow.cs
+++ Game/Assets/Resources/Scripts/CameraFollow.cs
@@ -6,16 +6,21 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform playerTarget;
     public float cameraSpeed = 7.5f;
-    public Vector3 offset;
-    Vector3 kameraPos;
+    public Vector3 batas;
+    Transform kameraPos;
+
+    void Start()
+    {
+        kameraPos = GetComponent<Transform>();
+    }
 
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        Vector3 playerPos = playerTarget.position + offset;
+        Vector3 playerPos = playerTarget.position + batas;
         Vector3 smoothPos = Vector3.Lerp(transform.position, playerPos, cameraSpeed * Time.deltaTime);
         transform.position = smoothPos;
-
+        
         transform.LookAt(playerTarget);
 	}
 }
