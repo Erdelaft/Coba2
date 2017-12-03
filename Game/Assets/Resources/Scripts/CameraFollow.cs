71daaf2c21caee8214ec896821083000aca6b87e
krlan2789 <33871129+krlan2789@users.noreply.github.com>
2017-12-03 10:23:35 +0700

    Sak onok e

--- Game/Assets/Resources/Scripts/CameraFollow.cs
+++ Game/Assets/Resources/Scripts/CameraFollow.cs
@@ -7,12 +7,33 @@ public class CameraFollow : MonoBehaviour
     public Transform playerTarget;
     public float cameraSpeed = 7.5f;
     public Vector3 batas;
+    int countTouched = 0;
+    Vector3 playerPos, smoothPos;
     
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    /*
+    Vector3 KameraRotasi(int n)
+    {
+        // kameraRotasi = transform.RotateAround(playerPos, transform.rotation.eulerAngles, 5f);
+
+        return kameraRotasi;
+    }
+    */
+
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        Vector3 playerPos = playerTarget.position + batas;
-        Vector3 smoothPos = Vector3.Lerp(transform.position, playerPos, cameraSpeed * Time.deltaTime);
+        playerPos = playerTarget.position + batas;
+        smoothPos = Vector3.Lerp(transform.position, playerPos, cameraSpeed * Time.deltaTime);
         transform.position = smoothPos;
         
         transform.LookAt(playerTarget);
--- Game/Assets/Resources/Scripts/Mode/AmbilSampah.cs
+++ Game/Assets/Resources/Scripts/Mode/AmbilSampah.cs
@@ -7,11 +7,13 @@ public class AmbilSampah : MonoBehaviour
 {
     public RectTransform objekRange;
     public Image areaImage;
-    public int jumlahSampah;
     public GameObject[] objekSampah;
-    public float delayMuncul = 12f;
+    public float delayMuncul = 12f, lokasiSampah = 1.5f;
+    public int[] rangeJumlahSampah;
     float areaSampahX;
     float areaSampahZ;
+    int jumlahSampah;
+    int n;
 
     // Use this for initialization
     void Start ()
@@ -20,16 +22,15 @@ public class AmbilSampah : MonoBehaviour
         areaSampahX = objekRange.rect.max.x;
         areaSampahZ = objekRange.rect.max.y;
 
-        jumlahSampah = Random.Range(15, 30);
+        jumlahSampah = Random.Range(rangeJumlahSampah[0], rangeJumlahSampah[1]);
         
         for(int i = 0; i < jumlahSampah; i++)
         {
             num = Random.Range(0, objekSampah.Length);      // Random objek sampah
 
-            Instantiate(objekSampah[num], new Vector3(Random.Range(-areaSampahX, areaSampahX), 3f, Random.Range(-areaSampahZ, areaSampahZ)), Quaternion.identity);
+            Instantiate(objekSampah[num], new Vector3(Random.Range(-areaSampahX, areaSampahX), lokasiSampah, Random.Range(-areaSampahZ, areaSampahZ)), Quaternion.identity);
         }
-
-        //Destroy(objekSampah[num], 13f * Time.deltaTime);
+        
         Debug.Log("x = " + Random.Range(4f, areaSampahX) + "/nz = " + Random.Range(4f, areaSampahZ));
     }
 	
