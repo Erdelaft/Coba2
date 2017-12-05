7465b66c3f765dd7d7c86c5c960a30ff0a820812
krlan2789 <33871129+krlan2789@users.noreply.github.com>
2017-12-05 12:59:21 +0700

    Fix Day Night

--- Game/Assets/Resources/Scripts/RealTimeClock.cs
+++ Game/Assets/Resources/Scripts/RealTimeClock.cs
@@ -7,14 +7,24 @@ public class RealTimeClock : MonoBehaviour
 {
     public Text teksWaktu;
     public Transform sunTransform;
-    public Light sun;
-    float time, intensity;
+    public Light sun;   // 1
+
+    [SerializeField]
+    [Range(0, 1)]
+    public float time = 0;
+    float currentTime = 1f, intensity, fullday = 120f;         // 4, 5, 2
     //int days;
     Color fogDay = Color.grey, fogNight = Color.black;
     
+    void Start()
+    {
+        intensity = sun.intensity;
+    }
+
     // Update is called once per frame
     void Update ()
     {
+        
         string waktu = waktuSekarang();
 
         string[] tempWaktu = waktu.Split(":"[0]);
@@ -24,10 +34,10 @@ public class RealTimeClock : MonoBehaviour
         else
             teksWaktu.text = tempWaktu[0] + ":" + tempWaktu[1];
 
-        time = Convert.ToInt32(tempWaktu[0]) * Convert.ToInt32(tempWaktu[1]) * 60;
+        time = (Convert.ToInt32(tempWaktu[0]) * 3600f) + (Convert.ToInt32(tempWaktu[1]) * 60f);
 
         Debug.Log("Time : " + time);
-
+        
         sunTransform.rotation = Quaternion.Euler(new Vector3((time - 21600) / 86400 * 360, 0, 0));
 
         if (time < 43200)
@@ -42,8 +52,44 @@ public class RealTimeClock : MonoBehaviour
         RenderSettings.fogColor = Color.Lerp(fogNight, fogDay, intensity * intensity);
 
         sun.intensity = intensity;
-    }
+        
 
+        // Version 2
+        /*
+        UpdateSun();
+
+        time += (Time.deltaTime / fullday) * currentTime;
+
+        if(time >= 1)
+        {
+            time = 0;
+        }
+        */
+
+    }
+/*
+    void UpdateSun()
+    {
+        sun.transform.localRotation = Quaternion.Euler((time * 360f) -90, 170, 0);
+
+        float intensityMultiplier = 1f;
+
+        if(time <= 0.23f || time >= 0.75f)
+        {
+            intensityMultiplier = 0;
+        }
+        else if(time <= 0.25f)
+        {
+            intensityMultiplier = Mathf.Clamp01((time - 0.23f) * (1 / 0.02f));
+        }
+        else if (time >= 0.75f)
+        {
+            intensityMultiplier = Mathf.Clamp01(1 - (time - 0.73f) * (1 / 0.02f));
+        }
+
+        sun.intensity = intensityMultiplier * intensity;
+    }
+*/
     string waktuSekarang()
     {
         DateTime sekarang = DateTime.Now;
